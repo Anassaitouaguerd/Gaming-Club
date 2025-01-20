a5664f2af0456befb28674b25d3b2b1ef269cfac
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2025-01-20 11:08:53 +0100

    interface service user

--- user/src/main/java/com/user_service/user/service/interfaces/UserInterface.java
+++ user/src/main/java/com/user_service/user/service/interfaces/UserInterface.java
@@ -1,4 +1,14 @@
 package com.user_service.user.service.interfaces;
 
+import com.user_service.user.dto.user.UserDTO;
+import com.user_service.user.entity.User;
+
+import java.util.List;
+
 public interface UserInterface {
+    public User addUser(UserDTO userDTO);
+    public User updateUser(Long userID , UserDTO userDTO);
+    public void deleteUser(Long userID);
+    public List<User> getAllUsers();
+
 }
